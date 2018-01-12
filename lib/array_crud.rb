b1792465f62ce32f6d31afabe612db85c016b2eb
Jenn Cacace <jenn.cacace@gmail.com>
2018-01-12 22:54:50 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  girl_names = ["Samantha", "Nancy", "Valentina", "Georgia", "Paulina"]
+  girl_names = ["Samantha", "Nancy", "Valentina", "Georgia"]
 end
 
 def add_element_to_end_of_array(array, element)
